809ff60af2b6a9b1783c3f0ac580e5762d8816b2
Ayesha14786 <ayesa.farsana@lmnas.com>
2024-05-17 12:31:54 +0530

    pushed after performing eslint

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+export default [
+    {
+      languageOptions: {
+        globals: {
+          frappe: "readonly", // Define frappe as a global variable
+          frm: "readonly", // Define frm as a global variable
+          doc: "readonly",
+          __: "readonly" // Ignore usage of __
+        }
+      },
+      rules: {
+        semi: ["error", "always"],
+        indent: ["warn", "tab"],
+        "comma-dangle": ["warn", "never"],
+        "no-unused-vars": "off",
+        camelcase: ["error", { properties: "never" }],
+        "no-param-reassign": ["error", { props: false }],
+        "no-shadow": "error",
+        "no-restricted-syntax": [
+          "error",
+          {
+            selector: "VariableDeclarator[init.type='ArrayExpression']",
+            message: "Use 'la' prefix for array variables",
+          },
+          {
+            selector: "VariableDeclarator[init.type='ObjectExpression']",
+            message: "Use 'ld' prefix for object variables",
+          },
+          {
+            selector: "FunctionDeclaration > Identifier",
+            message: "Function names should start with 'im' and follow camelCase",
+          },
+          {
+            selector: "FunctionDeclaration",
+            message: "Function names should start with 'fn' and follow camelCase",
+          },
+        ],
+        "no-invalid-this": "off",
+        "no-console": "off",
+      },
+    },
+  ];
