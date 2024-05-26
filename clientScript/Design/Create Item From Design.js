a0efe3605668482bf855185099fd7516cba12a56
Ayesha14786 <ayesa.farsana@lmnas.com>
2024-05-26 10:39:40 +0530

    removed dead code

--- clientScript/Design/Create Item From Design.js
+++ clientScript/Design/Create Item From Design.js
@@ -1,21 +1,7 @@
 frappe.ui.form.on('Design', {
 	refresh(frm) {
 	    if (frm.doc.status == 'Calculation Received') {
-	       // frappe.call({
-                // method: 'frappe.desk.form.linked_with.get_linked_docs',
-                // args: {
-                //     doctype: 'Item',
-                //     name: frm.docname
-                // },
-  
-                // method: 'frappe.desk.form.linked_with.get',
-                // args: {
-                //     doctype: 'Item',
-                //     docname: frm.docname
-                // },
-                // callback: function(response) {
-                //     console.log('message from get_linked', response.message);
-                // }});
+	       
 	        if (frm.doc.item){
 	            //Add the "Create Item" button in Sales Order
         	    frm.add_custom_button(__("View Item"), function() {
@@ -64,8 +50,6 @@ frappe.ui.form.on('Design', {
                                         });
                 		    })
                             
-                		      //  var delivery_note = response.message.parent;
-                		      //  frappe.set_route('delivery-note', delivery_note);
                 		    }else{                        
                 		        frappe.show_alert({
                                     message:__('Error Creating Item'),
