86be67fc41ffec4f74aa18201b75f6441c3cf3dd
Ayesha14786 <ayesa.farsana@lmnas.com>
2024-07-26 11:25:30 +0530

    workflow -added offer-workflow branch

--- clientScript/Quotation/Quotation Workflow Controls.js
+++ clientScript/Quotation/Quotation Workflow Controls.js
@@ -1,10 +1,32 @@
 frappe.ui.form.on('Quotation', {
 	refresh(frm) {
 		setTimeout(() => {
-            // frm.remove_custom_button('Update Items');
-            // frm.remove_custom_button('Close', 'Status');
-            // frm.remove_custom_button('Work Order', 'Make');
-			frm.clear_custom_buttons();
+		    
+		    if(frm.doc.workflow_state != "Approved") {
+
+    			frm.clear_custom_buttons();
+    		
+                $("button[data-original-title=" + __("Print") + "]").hide();
+                
+                $('a.grey-link').each(function() {
+                  
+                    // Comparing data-label instead of Button Text
+                    var id = $(this).children(':first-child').attr('data-label'); 
+                  
+                    switch(id){
+                        case __("Print"):
+                             $(this).remove();
+                             break;
+                        case __("Email"):
+                             $(this).remove();
+                             break;
+                        default:
+                            break;
+                            
+                    }
+                });
+		    }
         }, 10);
+		
 	}
 })
