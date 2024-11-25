0d6b1ec3c504a4b39defea6e9dd5e2730d86d36a
Eswari-Manie <eswari.maniyarasan@lmnas.com>
2024-11-25 12:36:14 +0530

    design not save

--- clientScript/Design/Design Select Fields Option.js
+++ clientScript/Design/Design Select Fields Option.js
@@ -1,3 +1,7 @@
+// Change Reference
+// Create the new design without follow the exist filters:>>(ISS-2024-00133)
+// Design form saved the twice:>>(ISS-2024-00133)
+
 frappe.ui.form.on('Design', {
     fngetAttributeOptionAndDefault(frm, iAttributeLabel, 
             iAttributeName, iTransformerType, iIsDesign, iReset = false) {
@@ -83,7 +87,9 @@ fnSetOptionsAndDefault(frm, iReset = false) {
         });
     },
 
-    refresh(frm) {
+    //refresh(frm) { //Commented this line for the issue (<<ISS-2024-00133)
+    onload(frm) {
+        //Issue: Form saved twice in the draft status (>>ISS-2024-00133)
         frm.events.fnSetOptionsAndDefault(frm);
     },
 
@@ -97,6 +103,14 @@ fnSetOptionsAndDefault(frm, iReset = false) {
 
     transformer_type(frm) {
         frm.events.fnSetOptionsAndDefault(frm);
-    }
+    },
+    //Clear the exist status filters when enter into the new design form >>(ISS-2024-00133)
+    //Reseting the framework functionality of carring 
+    //the filter value to the full form on creating new document
+    refresh(frm) {
+	    if(frm.is_new() && frm.is_dirty()){
+	        frm.set_value('status', 'Draft');
+	    }
+	}
 
 });
