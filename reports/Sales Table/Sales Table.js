1b1c85196340c5c10f8e9a4e6a74501066c5d64e
Eswari-Manie <eswari.maniyarasan@lmnas.com>
2025-01-09 11:12:02 +0530

    Sale table and retrain uk value

--- reports/Sales Table/Sales Table.js
+++ reports/Sales Table/Sales Table.js
@@ -1,9 +1,13 @@
 // Change References
 // Saved Column:(Issue# : ISS-2024-00005)
-// Remove the columns filter when there is no data for the filter(Issue# : ISS-2024-00008)
+// Remove the columns filter when there is 
+// no data for the filter(Issue# : ISS-2024-00008)
 // Some Columns not visible using Saved Column (Issue# : ISS-2024-00020)
 // User session defaults functionality is not working (Issue# : ISS-2024-00085)
-//filters for trafo type, uncheck open on load and remove month and date filter (US-2024-0141)
+// filters for trafo type, uncheck open on load 
+// and remove month and date filter (US-2024-0141)
+// Red mark shown incorrectly 
+// when applying column filter in sales table (Problem Ticket: ISS-2024-00062)
 
 frappe.query_reports['Sales Table'] = {
     "filters": [
@@ -73,7 +77,8 @@ frappe.query_reports['Sales Table'] = {
         //         return laMonths;
         //     }
         // },
-        //trafo type filter for all factories (US-2024-0141)
+
+        // Include trafo type filter for all factories (US-2024-0141)
         {
             "fieldname": "trafo_type",
             "label": __("Trafo Type"),
@@ -102,28 +107,41 @@ frappe.query_reports['Sales Table'] = {
             "fieldtype": "Check",
             "default": false  //<<US-2024-0141 unchecked as default
         }
-        
     ],
+
+    // Display the checkbox as a first column in the report 
+    // to allow selection and viewing of the specified sales order details.
     get_datatable_options: function(options) {
         return Object.assign(options, {
             checkboxColumn: true
         });
     },
+
+    // To modify the display of values in a table column 
+    // by using the custom formatter function. 
+    // It takes the value, row, column, and data as inputs 
+    // and applies the styles based on the condition
+    // Finally, the function returns the formatted value 
+    // wrapped in HTML to ensure the changes are reflected.
     "formatter": function(value, row, column, data, default_formatter) {
         value = default_formatter(value, row, column, data);
         // Wrap an empty div. Otherwise Total text will display as undefined
 		value = $(`<div>${value}</div>`);
 		// Make the entire total row bold
 		var $value = $(value).css("font-weight", "normal"); 
-		// Set the background colour of empty RDG Number on 18 Jul
-		if (data && column.fieldname === "rdg_number" && (!data.rdg_number || data.rdg_number === "no")) {
+		// Set the background colour as red When the RDG Number is empty
+		if (data && column.fieldname === "rdg_number" && 
+            (!data.rdg_number || data.rdg_number === "no")) {
             $value.addClass('bg-danger text-black w-100 h-100');
         }
-        // Set the background colour of empty Serial Number on 18 Jul
-        if (data && column.fieldname === "serial_number" && (!data.serial_number || data.serial_number === "Not available")) {
+        // Set the background colour as red when Serial Number is empty
+        if (data && column.fieldname === "serial_number" && 
+            (!data.serial_number || data.serial_number === "Not available")) {
             $value.addClass('bg-danger text-black w-100 h-100');
         }
-        // Set the background colour of Transformer Status on 18 Jul
+        // Set the background color to green 
+        // when the Transformer Status is 'Done-Expedited' 
+        // and to yellow when the Transformer Status is 'Done-OL Stock'.
 		if (data && column.fieldname === "transformer_status") {
             if (data.transformer_status === "Done - Expedited") {
                 $value.addClass('bg-success text-black w-100 h-100');
@@ -131,12 +149,32 @@ frappe.query_reports['Sales Table'] = {
                 $value.addClass('bg-warning text-black w-100 h-100');
             }
         }
- 
-		// Wrap the component with empty paragraph. Otherwise the changes will not reflect
+
+        //>>ISS-2024-00062
+        // When applying a column filter with a value 
+        // for 'rdg_number' or 'serial_number',
+        // the color should only be applied to empty rows, 
+        // not to rows with values..
+        if (column.fieldname === "rdg_number" || 
+            column.fieldname === "serial_number") {
+            // If the value is empty or contains only whitespace,
+            // apply a red background
+            if (!value || !value.text().trim()) {
+                $value.addClass("bg-danger text-white");
+            } else {
+                // Remove colors for non-empty rows in 
+                // both rdg number and serial number
+                $value.removeClass("bg-danger text-white");
+            }
+        } //<<ISS-2024-00062
+        
+		// Wrap the component with empty paragraph.
+        // Otherwise the changes will not reflect
 		value = $value.wrap("<p></p>").parent().html();
 		
         return value;
     },
+
     // >> ISS-2024-00005
     //Uncomment this section lines of code
     "onload": function (report) {
@@ -165,19 +203,19 @@ frappe.query_reports['Sales Table'] = {
                     "async": false,
             "callback": (iResponse)=> {
                 if(iResponse.message){
-                        // Set the from date fetched from user session to the report filter
-                        report.set_filter_value("from_date", iResponse.message?.from_date ? iResponse.message.from_date : frappe.datetime.month_start());
-                        // Set the to date fetched from user session to the report filter
-                        report.set_filter_value("to_date", iResponse.message?.to_date ? iResponse.message.to_date : frappe.datetime.month_end());
-                            }
+                    // Set the from date fetched from user session 
+                    // to the report filter
+                    report.set_filter_value("from_date", iResponse.message?.from_date ? iResponse.message.from_date : frappe.datetime.month_start());
+                    // Set the to date fetched from user session 
+                    // to the report filter
+                    report.set_filter_value("to_date", iResponse.message?.to_date ? iResponse.message.to_date : frappe.datetime.month_end());
                     }
+                }
         });
         // >> ISS-2024-00085
 
         // Add inner button to the report page
         report.page.add_inner_button(__("Save Columns"), function() {
-            // Get the values of the report
-            var lFilters = report.get_values();
             var laTextArray=[];
             // >> ISS-2024-00020
             // Iterate through header columns (assuming a maximum of 69 columns)
@@ -191,51 +229,49 @@ frappe.query_reports['Sales Table'] = {
                     // and store it in TextArray.
                     const TEXT = ELEMENT.innerText;
                     laTextArray.push(TEXT);
-                   }
                 }
-                // Whenever empty spaces appear in the header element, use the <br> tag
-                // to prevent text skipping
-                var laColumnArray = laTextArray.map(function(ELEMENT) {
-                    return ELEMENT.replace(/\n/g, '<br>');
-                });
-                // Convert the array values into a JSON string to store them as
-                // a single value in the database
-                const COLUMN_ORDER_ARRAY=JSON.stringify(laColumnArray);
-                //Set the modified columns as the report columns for the specific user
-                frappe.call({
-                    "method":"frappe.client.set_value",
-                     "args":{
-                        "doctype":"User Session Defaults",
-                        "name":frappe.session.user,
-                        "fieldname":"report_columns",
-                        "value":COLUMN_ORDER_ARRAY
-                    },
-                // Display a success message when the columns are successfully 
-                // updated otherwise shows the error mesaage.
-                "callback": (r)=> {
-                if(r.message){
-                        frappe.show_alert({
-                                    message:__('Column Preference Saved'),
-                                    indicator:'green'
-                                }, 5);
-                            }
-                    },
-                "error": (r)=>{
-                        if(r.message){
-                           frappe.show_alert({
-                                    message:__('Failed to Save'),
-                                    indicator:'Red'
-                                }, 5);
-                        }
-                    }
-                })
+            }
+            // Whenever empty spaces appear in the header element, 
+            // use the <br> tag to prevent text skipping
+            var laColumnArray = laTextArray.map(function(ELEMENT) {
+                return ELEMENT.replace(/\n/g, '<br>');
             });
-    
+            // Convert the array values into a JSON string to store them as
+            // a single value in the database
+            const COLUMN_ORDER_ARRAY=JSON.stringify(laColumnArray);
+            // Set the modified columns as the report columns 
+            // for the specific user
+            frappe.call({
+                "method":"frappe.client.set_value",
+                    "args":{
+                    "doctype":"User Session Defaults",
+                    "name":frappe.session.user,
+                    "fieldname":"report_columns",
+                    "value":COLUMN_ORDER_ARRAY
+                },
+            // Display a success message when the columns are successfully 
+            // updated otherwise shows the error mesaage.
+            "callback": (response)=> {
+                if(response.message){
+                    frappe.show_alert({
+                                message:__('Column Preference Saved'),
+                                indicator:'green'
+                        }, 5);
+                    }
+                },
+                "error": (response)=>{
+                    if(response.message){
+                        frappe.show_alert({
+                                message:__('Failed to Save'),
+                                indicator:'Red'
+                            }, 5);
+                    }
+                }
+            });
+        });
     }
     //<< ISS-2024-00005
-        
 };
-
 
 //Highlight the selected checkbox row in the Sales Order by shading it dark.
 //Fix the first two columns in the Sales table report to facilitate
@@ -243,8 +279,8 @@ frappe.query_reports['Sales Table'] = {
 //Column filters do not work when no filter data is found in the list.
 // Then set the width for the scroll bar.
 //>> ISS-2024-00008
-const style = document.createElement('style');
-style.innerHTML = `
+const STYLE = document.createElement('style');
+STYLE.innerHTML = `
     .dt-row--highlight .dt-cell {
         background-color: #fffce7;
         background-color: var(--dt-selection-highlight-color);
@@ -267,7 +303,6 @@ style.innerHTML = `
         z-index: 2;
         left: 0px; /* Adjust this value to match the width of col-1 */
     }
-
     .dt-instance-1 .dt-cell--col-1 {
         display: flex;
         text-align: right;
@@ -279,8 +314,6 @@ style.innerHTML = `
         position: absolute !important;
         width: 6829px !important;
     }
-    
-
 `;
-document.head.appendChild(style); 
+document.head.appendChild(STYLE); 
 //>>ISS-2024-00008
