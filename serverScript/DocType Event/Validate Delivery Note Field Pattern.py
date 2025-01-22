59143d4eb6be22357a85eea9143ee40e1871d1fb
Eswari-Manie <eswari.maniyarasan@lmnas.com>
2025-01-22 15:25:00 +0530

    coding standard SAP rework

--- serverScript/DocType Event/Validate Delivery Note Field Pattern.py
+++ serverScript/DocType Event/Validate Delivery Note Field Pattern.py
@@ -1,43 +1,53 @@
-def validate_serial_number_duplicate(serial_number, schedule, doc):
-    if serial_number:
-        duplicate = frappe.db.exists(
-            "Delivery Schedule", {"serial_number": serial_number, "docstatus": 1}
+def fn_validate_serial_number_duplicate(i_serial_number, i_schedule, doc):
+    # Validate if the serial number exists in a confirmed Delivery Schedule
+    if i_serial_number:
+        l_duplicate = frappe.db.exists(
+            "Delivery Schedule", {"serial_number": i_serial_number, "docstatus": 1}
         )
-        if duplicate:
+        # Check if the serial number exists in any other confirmed Delivery Schedules
+        if l_duplicate:
             # Check if the serial number is present in other Delivery Note schedules
-            count = frappe.db.count(
+            l_count = frappe.db.count(
                 "Delivery Schedule",
-                {"serial_number": serial_number, "parent": ["!=", schedule.parent]},
+                {"serial_number": i_serial_number, "parent": ["!=", i_schedule.parent]},
             )
             # If there is an entry raise error
-            if count > 0:
+            if l_count > 0:
                 raise frappe.ValidationError("Duplicate Serial Number")
     # Check if the Serial number is present in schedule lines of current document
-    count = sum(
+    l_count = sum(
         1
         for schedule_item in doc.delivery_schedule
-        if schedule_item.serial_number == serial_number
+        if schedule_item.serial_number == i_serial_number
     )
     # If there are more than one entries then raise error
-    if count > 1:
+    if l_count > 1:
         raise frappe.ValidationError("Duplicate Serial Number")
-    
+
+
+# Call the API function as "validate_document_fields" for validating
+# Serial number and invoice number
 lo_response = frappe.call("validate_document_fields", io_doc=doc)
 la_error_messages = []
+# Get all the reponse messages from the API and
+# add the error message to the error message list
 if lo_response.get("message"):
     for l_message in lo_response["message"]:
+        # Add error messages to the list if they indicate failure.
         if l_message["code"] != 200:
-            la_error_messages.append(l_message["msg"])
+            la_error_messages.append(l_message["i_msg"])
+
+    # If there are error messages, throw an error with the list of messages
     if len(la_error_messages) > 0:
         frappe.throw(title="Error", msg=la_error_messages, as_list=True)
 
-for schedule in doc.delivery_schedule:
-    # if schedule.invoice_number:
-    #     frappe.call('validate_naming_pattern', prefix='781', length=9, field_value=schedule.invoice_number, field='Invoice Number')
-    if schedule.serial_number:
-        # frappe.call('validate_naming_pattern', prefix='70', length=7, field_value=schedule.serial_number, field='Serial Number')
+for i_schedule in doc.delivery_schedule:
+    # if i_schedule.invoice_number:
+    #     frappe.call('validate_naming_pattern', prefix='781', length=9, field_value=i_schedule.invoice_number, field='Invoice Number')
+    if i_schedule.serial_number:
+        # frappe.call('validate_naming_pattern', prefix='70', length=7, field_value=i_schedule.serial_number, field='Serial Number')
         try:
-            validate_serial_number_duplicate(schedule.serial_number, schedule, doc)
+            fn_validate_serial_number_duplicate(i_schedule.serial_number, i_schedule, doc)
         except Exception as e:
-            title = ("Serial Number:" + schedule.serial_number,)
+            title = ("Serial Number:" + i_schedule.serial_number,)
             frappe.throw(title=title, msg=str(e))
--- serverScript/DocType Event/Validate Sales Order Field Pattern Submitted.py
+++ serverScript/DocType Event/Validate Sales Order Field Pattern Submitted.py
@@ -1,11 +1,15 @@
+# Call the API function as "validate_document_fields" for validating
+# SAP reference and RDG number
 lo_response = frappe.call("validate_document_fields", io_doc=doc)
-
 la_error_messages = []
-
+# Get all the reponse messages from the API and
+# add the error message to the error message list
 if lo_response.get("message"):
-
     for l_message in lo_response["message"]:
+        # Add error messages to the list if they indicate failure.
         if l_message["code"] != 200:
-            la_error_messages.append(l_message["msg"])
+            la_error_messages.append(l_message["i_msg"])
+
+    # If there are error messages, throw an error with the list of messages
     if len(la_error_messages) > 0:
         frappe.throw(title="Error", msg=la_error_messages, as_list=True)
