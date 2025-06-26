5c13b743a26ddce1f311f0e0c34aaa588f16e05a
Fathima786Irfana <fathima.irfana@gmail.com>
2025-06-26 22:15:58 +0530

    Temperature and  js files removal of loops

--- clientScript/Design/Beta  Design Temperature Rise Defaults.js
+++ clientScript/Design/Beta  Design Temperature Rise Defaults.js
@@ -6,55 +6,55 @@ frappe.ui.form.on("Design", {
   onload(frm) {
       //Filtered the Design Configuration for story >>US-2025-0602
     if (frm.is_new()) {
-        if (!frm.doc.transformer_type) return;
-        const lDoctype = "Design Configuration";
-        // Initialize the model with doctype Design Configuration
-        frappe.model.with_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
-        "is_design": frm.doc.is_design}, function () {
-        let laValues = frappe.model.get_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
-          "is_design": frm.doc.is_design});
-        // <<US-2025-0602
-        // Set values from Gitra Settings to the form fields
-        frm.doc.temperature_rise_gitra = laValues.temperature_rise;
-        frm.doc.temperature_rise_datasheet = laValues.temperature_rise;
-        frm.doc.temperature_rise = laValues.temperature_rise;
-        frm.doc.ambient_max_temperature = laValues.ambient_max_temperature;
-        frm.doc.max_average_temperature_per_year =
-          laValues.max_average_temperature_per_year;
-        frm.doc.max_average_temperature_per_month =
-          laValues.max_average_temperature_per_month;
-        //incase of duplicating the design
-        //set the ip protection to 'IP00'
-        frm.doc.ip_protection = "IP00";
-        frm.refresh_fields();
+      if (!frm.doc.transformer_type) return;
+      const lDoctype = "Design Configuration";
+      // Initialize the model with doctype Design Configuration
+      frappe.model.with_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
+      "is_design": frm.doc.is_design}, function () {
+          let laValues = frappe.model.get_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
+            "is_design": frm.doc.is_design});
+          // <<US-2025-0602
+          // Set values from Gitra Settings to the form fields
+          frm.doc.temperature_rise_gitra = laValues.temperature_rise;
+          frm.doc.temperature_rise_datasheet = laValues.temperature_rise;
+          frm.doc.temperature_rise = laValues.temperature_rise;
+          frm.doc.ambient_max_temperature = laValues.ambient_max_temperature;
+          frm.doc.max_average_temperature_per_year =
+            laValues.max_average_temperature_per_year;
+          frm.doc.max_average_temperature_per_month =
+            laValues.max_average_temperature_per_month;
+          //incase of duplicating the design
+          //set the ip protection to 'IP00'
+          frm.doc.ip_protection = "IP00";
+          frm.refresh_fields();
       });
     }
   },
   is_design(frm){
     //   if (frm.is_new()) {
     //Filtered the Design Configuration story >>US-2025-0602
-        if (!frm.doc.transformer_type) return;
-        const lDoctype = "Design Configuration";
-        // Initialize the model with doctype Design Configuration
-        frappe.model.with_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
-        "is_design": frm.doc.is_design}, function () {
-        let laValues = frappe.model.get_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
-          "is_design": frm.doc.is_design});
-        //<<US-2025-0602
-        // Set values from Gitra Settings to the form fields
-        frm.doc.temperature_rise_gitra = laValues.temperature_rise;
-        frm.doc.temperature_rise_datasheet = laValues.temperature_rise;
-        frm.doc.temperature_rise = laValues.temperature_rise;
-        frm.doc.ambient_max_temperature = laValues.ambient_max_temperature;
-        frm.doc.max_average_temperature_per_year =
-          laValues.max_average_temperature_per_year;
-        frm.doc.max_average_temperature_per_month =
-          laValues.max_average_temperature_per_month;
-        //incase of duplicating the design
-        //set the ip protection to 'IP00'
-        // retain ip if toggle for same factory in is_design
-        // frm.doc.ip_protection = "IP00";
-        frm.refresh_fields();
+      if (!frm.doc.transformer_type) return;
+      const lDoctype = "Design Configuration";
+      // Initialize the model with doctype Design Configuration
+      frappe.model.with_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
+      "is_design": frm.doc.is_design}, function () {
+          let laValues = frappe.model.get_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
+            "is_design": frm.doc.is_design});
+          //<<US-2025-0602
+          // Set values from Gitra Settings to the form fields
+          frm.doc.temperature_rise_gitra = laValues.temperature_rise;
+          frm.doc.temperature_rise_datasheet = laValues.temperature_rise;
+          frm.doc.temperature_rise = laValues.temperature_rise;
+          frm.doc.ambient_max_temperature = laValues.ambient_max_temperature;
+          frm.doc.max_average_temperature_per_year =
+            laValues.max_average_temperature_per_year;
+          frm.doc.max_average_temperature_per_month =
+            laValues.max_average_temperature_per_month;
+          //incase of duplicating the design
+          //set the ip protection to 'IP00'
+          // retain ip if toggle for same factory in is_design
+          // frm.doc.ip_protection = "IP00";
+          frm.refresh_fields();
       });
     // }
   },
@@ -63,11 +63,11 @@ frappe.ui.form.on("Design", {
     if (!frm.doc.transformer_type) return;
     //Added for story <<US-2025-0602
     const lDoctype = "Design Configuration";
-        // Initialize the model with doctype Design Configuration
-        frappe.model.with_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
-        "is_design": frm.doc.is_design}, function () {
-        let laValues = frappe.model.get_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
-          "is_design": frm.doc.is_design});
+    // Initialize the model with doctype Design Configuration
+    frappe.model.with_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
+    "is_design": frm.doc.is_design}, function () {
+      let laValues = frappe.model.get_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
+        "is_design": frm.doc.is_design});
         //<<US-2025-0602
       frm.doc.ambient_max_temperature = laValues.ambient_max_temperature;
       frm.doc.max_average_temperature_per_year =
@@ -86,11 +86,11 @@ frappe.ui.form.on("Design", {
     if (!frm.doc.transformer_type) return;
     //Added for story <<US-2025-0602
     const lDoctype = "Design Configuration";
-        // Initialize the model with doctype Design Configuration
-        frappe.model.with_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
-        "is_design": frm.doc.is_design}, function () {
-        let laValues = frappe.model.get_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
-          "is_design": frm.doc.is_design});
+    // Initialize the model with doctype Design Configuration
+    frappe.model.with_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
+    "is_design": frm.doc.is_design}, function () {
+      let laValues = frappe.model.get_doc(lDoctype, {"transformer_type": frm.doc.transformer_type, 
+        "is_design": frm.doc.is_design});
         // <<US-2025-0602
       var lTemperatureDelta = Math.max(
         frm.doc[on_field] - laValues[on_field],
