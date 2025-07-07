f981bf6a106abce216e0619716978996374d09bb
Eswari-Manie <eswari.maniyarasan@lmnas.com>
2025-07-07 12:55:00 +0530

    Desgin configuration changes

--- clientScript/Design Configuration/Get the attributes and ranges.js
+++ clientScript/Design Configuration/Get the attributes and ranges.js
@@ -1,85 +1,105 @@
-frappe.ui.form.on('Design Configuration', {
-     onload(frm) {
-        // Check if form is new AND child table has rows => duplicated form
-        const LisDuplicateForm = frm.is_new() && frm.doc.attributes && frm.doc.attributes.length > 0;
-        console.log("duplicate",lIsDuplicateForm)
-    },
-    transformer_type(frm) {
-       
-        if (!frm.doc.transformer_type) {
-            frm.clear_table('attributes');
-            frm.refresh_field('attributes');
-            return;
-        }
-
-        // If duplicated form, do NOT update attributes on transformer_type change
-        if (LisDuplicateForm) {
-            console.log("Duplicate form: keep existing attributes");
-            return;
-        }
-        if (frm.doc.transformer_type) {
-
-            // Get the Item templates from the Item based on the Transformer Type
-            frappe.db.get_list('Item', {
-                filters: {
-                    item_name: frm.doc.transformer_type,
-                    has_variants: 1
-                },
-                fields: ['name']
-            }).then(laItems => {
-                if (laItems && laItems.length > 0) {
-                    const lItemName = laItems[0].name;
-
-                    frappe.model.with_doc('Item', lItemName, function() {
-                        const ldItemDoc = frappe.model.get_doc('Item', lItemName);
-                        
-                        // Extract attribute names (i.e.,parameters) from the item based on the trafo type
-                        if (ldItemDoc && ldItemDoc.attributes) {
-                            const laAttributes = ldItemDoc.attributes.map(ldAttr => ldAttr.attribute);
-                            console.log("Attributes for transformer_type item:", laAttributes);
-
-                            frm.clear_table('attributes');
-
-                            // Fetch the min and max ranges of the each attribute 
-                            // get from the Item attribute list based on the trafo type
-                            frappe.db.get_list('Item Attribute', {
-                                filters: {
-                                    attribute_name: ["in", laAttributes]
-                                },
-                                fields: ['attribute_name', 'from_range', 'to_range']
-                            }).then(laItemAttributes => {
-                                //Mapping the attribute names into the paramater field 
-                                laAttributes.forEach(ldAttribute => {
-                                    let ldChildRow = frm.add_child('attributes');
-                                    ldChildRow.parameter = ldAttribute;
-
-                                    // Find matching attribute in item_attributes list
-                                    const ldMatchingAttr = laItemAttributes.find(ldAttr => ldAttr.attribute_name === ldAttribute);
-                                    
-                                    // For each attribute name, create a child row, assign the attribute to the parameter field,
-                                    // and populate the min and max range values from the matching item attribute if found
-
-                                    if (ldMatchingAttr) {
-                                        ldChildRow.min = ldMatchingAttr.from_range;
-                                        ldChildRow.max = ldMatchingAttr.to_range;
-                                    }
-                                });
-
-                                frm.refresh_field('attributes');
-                            });
-                        }
-                    });
-                } else {
-                    // clear child table if no item found
-                    frm.clear_table('attributes');
-                    frm.refresh_field('attributes');
-                }
-            });
-        } else {
-            // clear child table if transformer_type cleared
-            frm.clear_table('attributes');
-            frm.refresh_field('attributes');
-        }
-
+frappe.ui.form.on("Design Configuration", {
+  onload(frm) {
+    // Check if form is new AND child table has rows => duplicated form
+    fnIsDuplicateForm(frm);
+  },
+  factory(frm) {
+    //When Switching the factory, clear all the fields from the form
+    frm.set_value("transformer_type", "");
+    frm.clear_table("attributes");
+    frm.refresh_field("attributes");
+    return;
+  },
+  transformer_type(frm) {
+    if (!frm.doc.transformer_type) {
+      frm.clear_table("attributes");
+      frm.refresh_field("attributes");
+      return;
     }
+
+    // If duplicated form, do NOT update attributes
+    // on transformer_type change
+    if (fnIsDuplicateForm(frm)) {
+      return;
+    }
+    if (frm.doc.transformer_type) {
+      // Get the Item templates from the Item
+      // based on the Transformer Type
+      frappe.db
+        .get_list("Item", {
+          filters: {
+            item_name: frm.doc.transformer_type,
+            has_variants: 1,
+          },
+          fields: ["name"],
+        })
+        .then((laItems) => {
+          if (laItems && laItems.length > 0) {
+            const LitemName = laItems[0].name;
+
+            frappe.model.with_doc("Item", LitemName, function () {
+              const LdItemDoc = frappe.model.get_doc("Item", LitemName);
+
+              // Extract attribute names (i.e.,parameters)
+              // from the item based on the trafo type
+              if (LdItemDoc && LdItemDoc.attributes) {
+                const LaAttributes = LdItemDoc.attributes.map(
+                  (ldAttr) => ldAttr.attribute
+                );
+
+                frm.clear_table("attributes");
+
+                // Fetch the min and max ranges of the each attribute
+                // get from the Item attribute list based on the trafo type
+                frappe.db
+                  .get_list("Item Attribute", {
+                    filters: {
+                      attribute_name: ["in", LaAttributes],
+                    },
+                    fields: ["attribute_name", "from_range", "to_range"],
+                  })
+                  .then((laItemAttributes) => {
+                    //Mapping the attribute names into the paramater field
+                    LaAttributes.forEach((ldAttribute) => {
+                      let ldChildRow = frm.add_child("attributes");
+                      ldChildRow.parameter = ldAttribute;
+
+                      // Find matching attribute in item_attributes list
+                      const LdMatchingAttr = laItemAttributes.find(
+                        (ldAttr) => ldAttr.attribute_name === ldAttribute
+                      );
+
+                      // For each attribute name, create a child row, 
+                      // assign the attribute to the parameter field,
+                      // and populate the min and max range values 
+                      // from the matching item attribute if found
+
+                      if (LdMatchingAttr) {
+                        ldChildRow.min = LdMatchingAttr.from_range;
+                        ldChildRow.max = LdMatchingAttr.to_range;
+                      }
+                    });
+
+                    frm.refresh_field("attributes");
+                  });
+              }
+            });
+          } else {
+            // clear child table if no item found
+            frm.clear_table("attributes");
+            frm.refresh_field("attributes");
+          }
+        });
+    } else {
+      // clear child table if transformer_type cleared
+      frm.clear_table("attributes");
+      frm.refresh_field("attributes");
+    }
+  },
 });
+
+// Used to retain the item attributes even 
+// if the transformer type differs within the same factory
+function fnIsDuplicateForm(frm) {
+  return frm.is_new() && frm.doc.attributes && frm.doc.attributes.length > 0;
+}
--- clientScript/Design Configuration/Item Template for Design Configuration.js
+++ clientScript/Design Configuration/Item Template for Design Configuration.js
@@ -1,115 +1,142 @@
-frappe.ui.form.on('Design Configuration', {
-    onload(frm) {
-
-        if (frm.doc.factory) {
-            fnfetchItemTemplateFromFactory(frm);
-        }
-        new MutationObserver(() => {
-            $("ul[role='listbox']").each(function () {
-            if (
-                $(this).prev(
-                "input[data-doctype='Design Configuration'][data-fieldname='transformer_type']"
-                ).length) {
-                    $(this).find("div[role='option']:has(i.fa-plus)").hide();
-                }
-            });
-        }).observe(document.body, { childList: true, subtree: true });
-    },
-    
-    factory: function(frm) {
-        fnfetchItemTemplateFromFactory(frm);
-    },
-
-    is_default: function(frm) {
-        fnCheckUniqueDesignAndDefault(frm, 'is_default', __('A default Design Configuration already exists for this Factory: '));
-    },
-
-    is_design: function(frm) {
-        fnCheckUniqueDesignAndDefault(frm, 'is_design', __('A Design Configuration marked as "is_design" already exists for this Transformer Type: '));
-    },
-    refresh(frm) {
-        if (!frm.is_new()) {
-            // Make fields read-only after save
-            frm.set_df_property('factory', 'read_only', 1);
-            frm.set_df_property('transformer_type', 'read_only', 1);
-            frm.set_df_property('is_design', 'read_only', 1);
-        } else {
-            // Allow editing while creating new document
-            frm.set_df_property('factory', 'read_only', 0);
-            frm.set_df_property('transformer_type', 'read_only', 0);
-            frm.set_df_property('is_design', 'read_only', 0);
-        }
+frappe.ui.form.on("Design Configuration", {
+  onload(frm) {
+    // Filter the Item template When the factory is present
+    if (frm.doc.factory) {
+      fnfetchItemTemplateFromFactory(frm);
     }
+    // Disable the Create a new Item option from the Transformer type field
+    new MutationObserver(() => {
+      $("ul[role='listbox']").each(function () {
+        if (
+          $(this).prev(
+            "input[data-doctype='Design Configuration'][data-fieldname='transformer_type']"
+          ).length
+        ) {
+          $(this).find("div[role='option']:has(i.fa-plus)").hide();
+        }
+      });
+    }).observe(document.body, { childList: true, subtree: true });
+  },
+
+  // Triggered when the 'factory' field value is changed
+  // Fetches the available item templates associated with the selected factory
+  factory: function (frm) {
+    fnfetchItemTemplateFromFactory(frm);
+  },
+
+  // Triggered when the 'is_default' checkbox is toggled
+  // Validates that only one Design Configuration is set as default per factory
+  // Throws an error if another default already exists for the selected factory
+  is_default: function (frm) {
+    fnCheckUniqueDesignAndDefault(
+      frm,
+      "is_default",
+      __("A default Design Configuration already exists for this Factory: ")
+    );
+  },
+
+  // Triggered when the 'is_design' checkbox is toggled
+  // Ensures only one configuration is marked as 
+  // 'is_design' for each transformer type
+  // Prevents duplication by validating existing records 
+  // with the same transformer type
+  is_design: function (frm) {
+    fnCheckUniqueDesignAndDefault(
+      frm,
+      "is_design",
+      __(
+        'A Design Configuration marked as "is_design" already exists for this Transformer Type: '
+      )
+    );
+  },
+
+  refresh(frm) {
+    if (!frm.is_new()) {
+      // Make fields read-only after save
+      frm.set_df_property("factory", "read_only", 1);
+      frm.set_df_property("transformer_type", "read_only", 1);
+      frm.set_df_property("is_design", "read_only", 1);
+    } else {
+      // Allow editing while creating new document
+      frm.set_df_property("factory", "read_only", 0);
+      frm.set_df_property("transformer_type", "read_only", 0);
+      frm.set_df_property("is_design", "read_only", 0);
+    }
+  },
 });
 
 /**
  * Checks if a given design configuration field is unique and default within a factory.
- * 
+ *
  * In each factory:- Only one design can be marked as default.
  * Each design can be enabled for only one transformer type.
- * 
- * This function verifies that the specified field (e.g., a default flag or design enabled flag)
+ *
+ * This function verifies that the specified field 
+ * (e.g., a default flag or design enabled flag)
  * is unique across the 'Design Configuration' documents for the same factory.
- * If another document already has this field set, it shows a message and resets the field.
+ * If another document already has this field set, 
+ * it shows a message and resets the field.
  */
 function fnCheckUniqueDesignAndDefault(frm, fieldName, iMessagePrefix) {
-    if (!frm.doc[fieldName] || !frm.doc.factory) return;
+  if (!frm.doc[fieldName] || !frm.doc.factory) return;
 
-    let ldFilters = {
-        factory: frm.doc.factory
-    };
-    ldFilters[fieldName] = 1;
+  let ldFilters = {
+    factory: frm.doc.factory,
+  };
+  ldFilters[fieldName] = 1;
 
-    if (fieldName === 'is_design') {
-        // Must also check transformer_type for is_design
-        if (!frm.doc.transformer_type) return;
-        ldFilters['transformer_type'] = frm.doc.transformer_type;
-    }
+  if (fieldName === "is_design") {
+    // Must also check transformer_type for is_design
+    if (!frm.doc.transformer_type) return;
+    ldFilters["transformer_type"] = frm.doc.transformer_type;
+  }
 
-    frappe.db.get_list('Design Configuration', {
-        filters: ldFilters,
-        fields: ['name'],
-        limit: 1
-    }).then(ldResponse => {
-        if (ldResponse.length && ldResponse[0].name !== frm.doc.name) {
-            frappe.msgprint(iMessagePrefix + ldResponse[0].name);
-            frm.set_value(fieldName, 0);
-        }
+  frappe.db
+    .get_list("Design Configuration", {
+      filters: ldFilters,
+      fields: ["name"],
+      limit: 1,
+    })
+    .then((ldResponse) => {
+      if (ldResponse.length && ldResponse[0].name !== frm.doc.name) {
+        frappe.msgprint(iMessagePrefix + ldResponse[0].name);
+        frm.set_value(fieldName, 0);
+      }
     });
 }
 
-// Fetches the item templates associated with the selected factory 
+// Fetches the item templates associated with the selected factory
 // and updates the transformer type options accordingly.
 function fnfetchItemTemplateFromFactory(frm) {
-    if (!frm.doc.factory) return;
+  if (!frm.doc.factory) return;
 
-    frappe.call({
-        method: "frappe.client.get",
-        args: {
-            doctype: "Factory",
-            name: frm.doc.factory
-        },
-        callback: function(ldResponse) {
-            if (ldResponse.message) {
-                const ldItemTemplateRows = ldResponse.message.item_template || [];
-                const laItemTemplateArray = ldItemTemplateRows.map(ldRow => ldRow.item_template);
-                fnsetTransformerTypeOptions(frm, laItemTemplateArray);
-                frm.refresh_field('transformer_type');
-            }
-        }
-    });
+  frappe.call({
+    method: "frappe.client.get",
+    args: {
+      doctype: "Factory",
+      name: frm.doc.factory,
+    },
+    callback: function (ldResponse) {
+      if (ldResponse.message) {
+        const LdItemTemplateRows = ldResponse.message.item_template || [];
+        const LaItemTemplateArray = LdItemTemplateRows.map(
+          (ldRow) => ldRow.item_template
+        );
+        fnsetTransformerTypeOptions(frm, LaItemTemplateArray);
+        frm.refresh_field("transformer_type");
+      }
+    },
+  });
 }
-
-// Sets a filter on the transformer_type field to only 
+// Sets a filter on the transformer_type field to only
 // show options matching the provided list of item templates.
 function fnsetTransformerTypeOptions(frm, laitems) {
-    frm.fields_dict.transformer_type.get_query = function(doc, cdt, cdn) {
-        return {
-            filters: [
-                ['Item', 'name', 'in', laitems],
-                ['Item', 'has_variants', '=', 1]
-            ]
-        };
+  frm.fields_dict.transformer_type.get_query = function (doc, cdt, cdn) {
+    return {
+      filters: [
+        ["Item", "name", "in", laitems],
+        ["Item", "has_variants", "=", 1],
+      ],
     };
+  };
 }
-
